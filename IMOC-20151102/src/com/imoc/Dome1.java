02f4d2c6261c3c144baf18d961bf61fe144a857d
赵常春 <zhaoccx@hotmail.com>
2015-11-04 00:39:29 +0800

    a demon

---
+++ IMOC-20151102/src/com/imoc/Dome1.java
@@ -0,0 +1,23 @@
+package com.imoc;
+
+import java.util.Scanner;
+
+public class Dome1 {
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		
+		
+		Scanner input = new Scanner(System.in);
+		int socre =input.nextInt();
+		
+		int count = 0;
+		System.out.println(socre);
+		
+		while(socre<60){
+			socre++;
+			count++;
+		}
+		System.out.println(socre);
+		System.out.println(count);
+	}
+}
