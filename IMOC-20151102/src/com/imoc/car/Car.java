104d2a6eedb781e4cad38349b0743298d70fce54
zhaoccx <zhaoccx@hotmail.com>
2015-11-26 00:08:16 +0800

    筷子

---
+++ IMOC-20151102/src/com/imoc/car/Car.java
@@ -0,0 +1,40 @@
+package com.imoc.car;
+
+public class Car {
+	private String	name;
+	private double	price;
+	private int		numman;
+	private int		numthing;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getNumman() {
+		return numman;
+	}
+
+	public void setNumman(int numman) {
+		this.numman = numman;
+	}
+
+	public int getNumthing() {
+		return numthing;
+	}
+
+	public void setNumthing(int numthing) {
+		this.numthing = numthing;
+	}
+}
