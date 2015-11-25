3dea6f322327312cc81e5fce089681ffa237547e
zhaoccx <zhaoccx@hotmail.com>
2015-11-25 22:53:02 +0800

    abstract

--- IMOC-20151102/src/com/imoc/peo/People.java
+++ IMOC-20151102/src/com/imoc/peo/People.java
@@ -6,10 +6,7 @@ package com.imoc.peo;
  * Purpose: Defines the Class People
  ***********************************************************************/
 /** @pdOid 929d7e82-19e6-46b1-833d-d6bff9f2b911 */
-public class People {
+public abstract class People {
 	/** @pdOid f2677db9-7f8d-44ba-8a7a-1af6c34b829c */
-	public int say() {
-		// TODO: implement
-		return 0;
-	}
+	public abstract int say();
 }
