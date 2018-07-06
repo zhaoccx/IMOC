3b568480c7c71102288a3673d12e46a7eee779da
趙常春 <zhaoccx@hotmail.com>
2018-07-06 12:52:47 +0800

    update

--- JavaBeansDemo/src/util/DBHelper.java
+++ JavaBeansDemo/src/util/DBHelper.java
@@ -4,12 +4,12 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBHelper {
-	private static final String	driver		= "com.mysql.jdbc.Driver";														// 数据库驱动
+	private static final String driver = "com.mysql.jdbc.Driver"; // 数据库驱动
 	// 连接数据库的URL地址
-	private static final String	url			= "jdbc:mysql://localhost:3306/test?useUnicode=true&characterEncoding=UTF-8";
-	private static final String	username	= "root";																		// 数据库的用户名
-	private static final String	password	= "5487102";																	// 数据库的密码
-	private static Connection	conn		= null;
+	private static final String url = "jdbc:mysql://localhost:3306/test?useUnicode=true&characterEncoding=UTF-8";
+	private static final String username = "root"; // 数据库的用户名
+	private static final String password = "root"; // 数据库的密码
+	private static Connection conn = null;
 	// 静态代码块负责加载驱动
 	static {
 		try {
